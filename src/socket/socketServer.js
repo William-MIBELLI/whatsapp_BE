2cb3cffae3eb95c852e58d01a5d3076fba06ceb9
William MIBELLI <william.mibelli@gmail.com>
2023-10-23 23:08:14 +0200

    socket for call event OK

--- src/socket/socketServer.js
+++ src/socket/socketServer.js
@@ -25,11 +25,13 @@ export default function (socket, io) {
         })
     })
 
+    //Quand un user est en train de taper
     socket.on('typing', conversationId => {
         console.log('typing OK : ', conversationId)
         socket.to(conversationId).emit('typing', conversationId)
     })
 
+    //Quand l'user ne tape plus
     socket.on('stop typing', conversationId => {
         console.log('stop typing : ', conversationId)
         socket.to(conversationId).emit('stop typing', conversationId)
@@ -41,6 +43,44 @@ export default function (socket, io) {
             return obj.socketId !== socket.id
         })
         onlineUsers = newOnlineUsers
+        console.log('DISCONNECT USER')
         io.emit('online-users', onlineUsers)
+    })
+
+    //Quand un user lance un appel
+    socket.on('start call', data => {
+
+        const { caller, receiver, signalData } = data
+
+        const user = onlineUsers.find(o => o.userId === receiver._id)
+
+        if (user) {
+            console.log('user trouvé ', user, receiver)
+            return socket.to(user.userId).emit('call incoming', caller, signalData)
+        }
+
+        console.log('utilisateur introuvable')
+    })
+
+
+    //on reçoit les infos de lutilisateur qui accepte l'appel ainbsi que son signal
+    socket.on('accept call', data => {
+        const { caller, signal } = data
+        
+        const user = onlineUsers.find(o => o.userId === caller._id)
+
+        if (user) {
+            socket.to(user.userId).emit('call accepted', signal)    //on emit le caller en lui passant le signal du receiver
+        }
+    })
+
+    //on récupère les infos de lutilisateur à notifier pour le call fini
+    socket.on('end call', (u) => {
+
+        const user = onlineUsers.find(o => o.userId === u._id)
+
+        if (user) {
+            socket.to(user.userId).emit('call ended')   // on lui emit simplement call ended, pas besoin de donnée
+        }
     })
 }
